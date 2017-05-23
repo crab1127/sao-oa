db2897b0f3e4870f3d06c13ddf7fb892e4fd1f4f
crab1127 <crab1127@gmail.com>
2017-05-23 11:13:41 +0800

    add 添加错误上报机制

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import VueResource from 'vue-resource'
 import YDUI from 'vue-ydui'
 import 'vue-ydui/dist/ydui.rem.css';
 
+import FE_DEBUG from 'error-report-crab'
+FE_DEBUG.init({
+  siteId: '47b2f28e-a516-4475-8629-57492b227bf6',
+  silent: process.env.NODE_ENV !== 'production'
+})
+Vue.config.errorHandler = function(err, vm) {
+  FE_DEBUG.reportError(err)
+}
+
 import App from './App'
 import router from './router'
 
@@ -23,7 +32,6 @@ Vue.http.interceptors.push(function(request, next) {
     }
   } catch (e) {}
   next((response) => {
-    console.log(response)
     if (response.status === 401) {
       if (response.body.errorCode != '00000') {
         location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa166ce145510c378&redirect_uri=http%3A%2F%2Fmpapi.redseawetoo.com%2Fuser%2Fauthorize%23&response_type=code&scope=SCOPE&state=STATE#wechat_redirect'
@@ -31,6 +39,26 @@ Vue.http.interceptors.push(function(request, next) {
     }
     if (response.status === 400 || response.status === 500) {
       alert('接口访问失败， 请联系管理员')
+    }
+    if (response.status >= 400 && response.status != 401) {
+      if (FE_DEBUG && FE_DEBUG.report) {
+        const params = {
+          type: 2,
+          fu: response.url,
+          referer: location.href,
+          stack: ''
+        }
+        request.params && (params.stack = 'params=' + request.params.toString())
+        request.body && (params.stack += 'body=' + request.body)
+
+        try {
+          params.msg = request.method + ' :: ' + response.status + ' :: ' + response.body.name + ',' + response.body.message
+
+        } catch (e) {
+          params.msg = request.method + ' :: ' + response.status
+        }
+        FE_DEBUG.report(params)
+      }
     }
   })
 })
